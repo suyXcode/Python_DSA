5f490d1d3b89f205a19dc1f8e97a453920105aeb
Suyash Singh <suyxcode@gmail.com>
2026-01-21 23:03:21 +0530

    Create Peak_index.py

---
+++ Arrays/Peak_index.py
@@ -0,0 +1,47 @@
+# 852. Peak Index in a Mountain Array
+
+# You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+
+# Return the index of the peak element.
+
+# Your task is to solve it in O(log(n)) time complexity.
+
+ 
+
+# Example 1:
+# Input: arr = [0,1,0]
+# Output: 1
+# Example 2:
+# Input: arr = [0,2,1,0]
+# Output: 1
+# Example 3:
+# Input: arr = [0,10,5,2]
+# Output: 1
+
+ 
+
+# Constraints:
+
+# 3 <= arr.length <= 105
+# 0 <= arr[i] <= 106
+# arr is guaranteed to be a mountain array.
+
+
+
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        low = 0
+        high = len(arr) - 1
+
+        while low < high:
+            mid = (low + high) // 2
+
+            if arr[mid] < arr[mid + 1]:
+                # peak is on the right side
+                low = mid + 1
+            else:
+                # peak is at mid or on the left side
+                high = mid
+
+        return low
