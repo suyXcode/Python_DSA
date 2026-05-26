d49bf7dfb6a7af5bfdbe49a42bb100fec3c971bf
Suyash Singh <suyxcode@gmail.com>
2026-05-26 10:19:01 +0530

    Create p0.py

---
+++ Linked_List/p0.py
@@ -0,0 +1,20 @@
+# 1. Reverse a Linked List
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head):
+        prev = None
+        curr = head
+
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        return prev
