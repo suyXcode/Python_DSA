5e922708d1dc8b916bcc3aaf28ee84d3916bfd1a
suyXcode <suyxcode@gmail.com>
2026-08-01 21:29:50 +0530

    Selection Sort

--- Sorting_Algorithms/selection_sort.py
+++ Sorting_Algorithms/selection_sort.py
@@ -1,4 +1,4 @@
-def selection_sort(nums):
+def selection_sort_asc(nums):
     print(f"Original array: {nums}")
     n=len(nums)
     for i in range(0,n):
@@ -10,4 +10,20 @@ def selection_sort(nums):
     print(f"Sorted array: {nums}")
 
 print("\n*** Selection Sort Algorithm in ascending order. ***\n")
-selection_sort([34, 12, 24, 9, 5])
+selection_sort_asc([34, 12, 24, 9, 5])
+
+
+
+def selection_sort_dsc(nums):
+    print(f"Original array: {nums}")
+    n=len(nums)
+    for i in range(0,n):
+        max_index=i
+        for j in range(i+1,n):
+            if nums[j]>nums[max_index]:
+                max_index=j 
+            nums[i],nums[max_index]=nums[max_index],nums[i]
+    print(f"Sorted array: {nums}")
+
+print("\n*** Selection Sort Algorithm in descending order. ***\n")
+selection_sort_dsc([34, 12, 24, 9, 5])
